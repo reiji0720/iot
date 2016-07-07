a3e67bc839776e0d8ce5a638032c406107df7fd3
れーじ <reizi720@gmail.com>
2016-07-07 14:55:38 +0900

    デザイン途中

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
   has_attached_file :itemphoto,
-                      styles:  { medium: "700x700#", thumb: "280x280#" },default_url: 'images/default.jpg'
+                      styles:  { medium: "700x700#", thumb: "280x280#" },default_url: 'assets/default.jpg'
   validates_attachment_content_type :itemphoto,
                                       content_type: ["image/jpg","image/jpeg","image/png"]
 
